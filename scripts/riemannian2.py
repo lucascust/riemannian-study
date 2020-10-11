bd9a85d91f39adb378433dec8ce0da42a3288e31
Lucas Custódio <lucascust@users.noreply.github.com>
2020-10-10 22:41:17 -0300

    knowledge update

--- scripts/riemannian2.py
+++ scripts/riemannian2.py
@@ -1,25 +1,41 @@
-import numpy as np
-import matplotlib.pyplot as plt
+# Some standard pythonic imports
+import warnings
+warnings.filterwarnings('ignore')
+import os,numpy as np,pandas as pd
+from collections import OrderedDict
+import seaborn as sns
+from matplotlib import pyplot as plt
 
-# mne import
-from mne import Epochs, find_events
-from mne.io import Raw, RawArray
+# MNE functions
+from mne import Epochs,find_events, filter
+from mne.io import Raw
+from mne.decoding import Vectorizer
 
-# pyriemann import
+
+# Scikit-learn and Pyriemann ML functionalities
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score, StratifiedShuffleSplit
 from pyriemann.estimation import Covariances
-# cross validation
-import pyriemann
-mdm = pyriemann.classification.MDM()
+from pyriemann.tangentspace import TangentSpace
+from pyriemann.classification import MDM
+# mdm = pyriemann.classification.MDM()
 
 
 raw = Raw("./data/subject12_run2_raw.fif", preload=True, verbose='ERROR')
+
+print(raw)
 
 events = find_events(raw, shortest_event=0, verbose=False)
 
 # Pega Channels baseado no parametro
 raw = raw.pick_types(eeg=True)
 
-#Bandpass filter
+#Bandpass filter, passar para cada frequencia
+# def _bandpass_filter(signal, lowcut, highcut):
+#     """ Bandpass filter using MNE """
+#     return signal.copy().filter(l_freq=lowcut, h_freq=highcut,
+#                                 method="iir").get_data()
 
 
 # Plot MNE
@@ -32,9 +48,11 @@ epochs = Epochs(raw, events, event_id, tmin=2, tmax=5, baseline=None)
 
 cov_raw = Covariances(estimator='lwf').transform(epochs.get_data())
 
+# Verificar necessidade de tirar as medias no fim
 
-#from sklearn.linear_model import LogisticRegression
-#var = epochs.get_data()
+
+# from sklearn.linear_model import LogisticRegression
+# var = epochs.get_data()
 # cov_raw = scikitlearn.pipeline(Covariances(estimator='lwf').transform(var), TangentSpace().transform(epochs.get_data(var), logisticRegression.fit(x,y,weight))
 
 #Tangent 
